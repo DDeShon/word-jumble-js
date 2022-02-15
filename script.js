997ced3e88644f037cf0f8f36de1de19c21ecf88
DDeShon <36036284+DDeShon@users.noreply.github.com>
2022-02-15 09:50:25 -0600

    working on javascript

--- script.js
+++ script.js
@@ -15289,3 +15289,42 @@ const dictionary = [
   "rural",
   "shave",
 ];
+
+function startInteraction() {
+  document.addEventListener("click", handleMouseClick);
+  document.addEventListener("keydown", handleKeyPress);
+}
+
+function handleMouseClick(e) {
+  if (e.target.matches("[data-key]")) {
+    pressKey(e.target.dataset.key);
+    return;
+  }
+
+  if (e.target.matches("[data-enter]")) {
+    submitGuess();
+    return;
+  }
+
+  if (e.target.matches("[data-delete]")) {
+    deleteKey();
+    return;
+  }
+}
+
+function handleKeyPress(e) {
+  if (e.key === "Enter") {
+    submitGuess();
+    return;
+  }
+
+  if (e.key === "Backspace" || e.key === "Delete") {
+    deleteKey();
+    return;
+  }
+
+  if (e.key.match(/^[a-z]$/)) {
+    pressKey(e.key);
+    return;
+  }
+}
